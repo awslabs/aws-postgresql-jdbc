d1ecd7b076d73232869dc2ac3351fecefed9ccf8
Jordan Wilson <jordanw@bitquilltech.com>
2021-03-30 16:10:11 -0700

    remove Travis step in Docker init

--- docker/scripts/init-user-db.sh
+++ docker/scripts/init-user-db.sh
@@ -16,4 +16,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     ALTER SYSTEM SET wal_level TO 'logical';
 EOSQL
 
-/home/scripts/travis_ssl_users.sh
+#/home/scripts/travis_ssl_users.sh
