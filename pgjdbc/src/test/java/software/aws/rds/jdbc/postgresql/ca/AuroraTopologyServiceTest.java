7772491a4ad0941ce1116b599704ae9645760701
congoamz <aarcongo@amazon.com>
2021-04-06 15:07:16 -0700

    Fix code style

--- pgjdbc/src/test/java/software/aws/rds/jdbc/postgresql/ca/AuroraTopologyServiceTest.java
+++ pgjdbc/src/test/java/software/aws/rds/jdbc/postgresql/ca/AuroraTopologyServiceTest.java
@@ -9,7 +9,6 @@ package software.aws.rds.jdbc.postgresql.ca;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
