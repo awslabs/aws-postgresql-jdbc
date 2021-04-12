d9aabd5f5dc333469714724e3a355aca8e298cb7
Sergiy Voshchykov <sergiyv@bitquilltech.com>
2021-04-12 12:05:47 -0700

    code style fix

--- pgjdbc/src/test/java/org/postgresql/test/jdbc2/LoginTimeoutTest2.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc2/LoginTimeoutTest2.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
