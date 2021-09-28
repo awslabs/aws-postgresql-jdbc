946c85b09742e62eb7798ecf4dbc7483d901b922
Karen Chen <karenc@bitquilltech.com>
2021-09-28 16:35:42 -0700

    chore: Refactor integration test.
    - Use a more generic connection string.
    - Add licence header.

--- pgjdbc/src/test/java/software/aws/rds/jdbc/postgresql/ca/AwsIamAuthenticationIntegrationTests.java
+++ pgjdbc/src/test/java/software/aws/rds/jdbc/postgresql/ca/AwsIamAuthenticationIntegrationTests.java
@@ -1,34 +1,44 @@
-package software.aws.rds.jdbc.postgresql.ca;
+/*
+ * AWS JDBC Driver for PostgreSQL
+ * Copyright Amazon.com Inc. or affiliates.
+ * See the LICENSE file in the project root for more information.
+ */
 
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.postgresql.PGProperty;
-import org.postgresql.util.PSQLException;
+package software.aws.rds.jdbc.postgresql.ca;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.postgresql.Driver;
+import org.postgresql.PGProperty;
+import org.postgresql.util.PSQLException;
+
 @Disabled
 public class AwsIamAuthenticationIntegrationTests {
-
-  private static final String CONNECTION_STRING_PREFIX = "jdbc:postgresql:aws://";
+  private static final String CONNECTION_STRING_PREFIX = "jdbc:postgresql://";
   private static final String CONNECTION_STRING = CONNECTION_STRING_PREFIX + System.getenv("PG_AURORA_INSTANCE_ENDPOINT");
   private static final String VALID_AWS_DB_USER = System.getenv("PG_AURORA_IAM_USER");
 
   @Test
   public void test_1_ValidAwsIamUser() throws SQLException {
+    Driver.register();
     final Properties properties = createProperties(VALID_AWS_DB_USER);
     Assertions.assertNotNull(DriverManager.getConnection(CONNECTION_STRING, properties));
+    Driver.deregister();
   }
 
   @Test
-  public void test_2_InvalidAwsIamUser() {
+  public void test_2_InvalidAwsIamUser() throws SQLException {
+    Driver.register();
     final Properties properties = createProperties("invalidUser", "invalidPass");
     Assertions.assertThrows(
         PSQLException.class,
         () -> DriverManager.getConnection(CONNECTION_STRING, properties));
+    Driver.deregister();
   }
 
   private Properties createProperties(final String user) {
